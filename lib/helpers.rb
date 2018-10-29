cb2d15e3fc95e49339e10cf2b7d5e14b646346d8
songziming <s.ziming@hotmail.com>
2018-10-30 04:17:48 +0800

    planning to show categories and tags index so we have to generate page for each category and tag

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 
-module MyHelper
+module PkbHelper
     def all_notes
         @items.find_all("/notes/**/*.md").map do |note|
             match  = note.identifier.to_s.match(/^\/notes\/(.*)\.md$/)
@@ -9,7 +9,24 @@ module MyHelper
             { :path => note.path, :titles => titles, :digest => digest }
         end
     end
+
     # todo: 遍历所有markdown，构建全局的目录结构，用来显示在网站左侧
+    def site_map
+        # 我们需要遵循用户指定的顺序，例如`02_系统`一定要排在第二位
+        # ruby默认的hash是有序的，但我们还要处理用户指定的编号重复的情况
+        all_notes.each do |note|
+        end
+    end
+
+    # turn hashmap into html list
+    def tree_to_list(toc)
+        toc.to_s
+    end
+
+    # generate toc from content
+    def note_toc(html)
+        "fake toc"
+    end
 end
 
-use_helper MyHelper
+use_helper PkbHelper
