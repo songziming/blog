1aeb10f831e9ea8f6994175126a434e7ed391822
songziming <s.ziming@hotmail.com>
2018-11-01 17:03:42 +0800

    modified site theme

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,32 +1,38 @@
 require 'nokogiri'
 
+# [:path, :compiled_content, :reps, :parent, :children, :binary?, :raw_filename, :==, :[], :eql?, :inspect, :hash, :fetch, :key?, :attributes, :reference, :identifier, :raw_content, :_unwrap, :frozen?, :_context, :to_yaml, :to_yaml_properties, :psych_to_yaml, :instance_of?, :public_send, :instance_variable_get, :instance_variable_set, :instance_variable_defined?, :remove_instance_variable, :private_methods, :kind_of?, :instance_variables, :tap, :method, :public_method, :singleton_method, :is_a?, :extend, :define_singleton_method, :to_enum, :enum_for, :<=>, :===, :=~, :!~, :respond_to?, :freeze, :display, :object_id, :send, :to_s, :nil?, :class, :singleton_class, :clone, :dup, :itself, :taint, :tainted?, :untaint, :untrust, :trust, :untrusted?, :methods, :protected_methods, :public_methods, :singleton_methods, :!, :!=, :__send__, :equal?, :instance_eval, :instance_exec, :__id__]
+
 module PkbHelper
-    def all_notes
-        @items.find_all("/notes/**/*.md").map do |note|
-            match  = note.identifier.to_s.match(/^\/notes\/(.*)\.md$/)
-            titles = match[1].split('/').map { |s| s.match(/^(\d+)-(.*)$/)[2] }
-            digest = note.compiled_content.gsub(/<\/?[^>]*>/, "").slice(0..127)
-            { :path => note.path, :titles => titles, :digest => digest }
-        end
-    end
-
-    # todo: 遍历所有markdown，构建全局的目录结构，用来显示在网站左侧
-    def site_map
-        # 我们需要遵循用户指定的顺序，例如`02_系统`一定要排在第二位
-        # ruby默认的hash是有序的，但我们还要处理用户指定的编号重复的情况
-        all_notes.each do |note|
-        end
-    end
-
-    # turn hashmap into html list
-    def tree_to_list(toc)
-        toc.to_s
+    # generate digest
+    def digest(note)
+        note.compiled_content.gsub(/<\/?[^>]*>/, '').slice(0..127)
     end
 
     # generate toc from content
     def note_toc(html)
-        "fake toc"
+        doc = Nokogiri::HTML::DocumentFragment.parse(html)
+        sel = (3..6).map(&-> (x) { "h#{x}[@id]" }).join('|')
+        hdr = doc.xpath(sel)
+        return if hdr.empty?
+        toc = ''
+        base  = hdr.map(&-> (h) { h.name.slice(1..-1).to_i }).min.to_i
+        level = base
+        hdr.each do |h|
+            l = h.name.slice(1..-1).to_i
+            if l > level
+                toc += '<ul>'  * (l - level)
+            elsif level > l
+                toc += '</ul>' * (level - l)
+            end
+            toc += "<li><a href=\"##{h[:id]}\">#{h.children}</a></li>"
+            level = l
+        end
+        if level > base
+            toc += '</ul>' * (level - base)
+        end
+        '<ul>' + toc + '</ul>'
     end
 end
 
 use_helper PkbHelper
+use_helper Nanoc::Helpers::Rendering
